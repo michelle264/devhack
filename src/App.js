b1c349cb56744255a8b48a237f3e9c870153dde3
yjliew666 <yjliew455@gmail.com>
2023-11-07 21:51:51 +0800

    App.js changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import UserProfile from './components/profile/UserProfile';
 import HRProfile from './components/profile/HRProfile';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
             <Route path="/userProfile" element={<UserProfile/>}></Route>
             <Route path="/HRProfile" element={<HRProfile/>}></Route>
+            <Route path="/Login" element={<Login/>}></Route>
             <Route path="*" element={<h1>Not Found</h1>}></Route>
 
           
