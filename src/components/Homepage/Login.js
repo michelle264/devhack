9970adb31a0b5bcdb7b1fd614c42e8a1efef253f
yjliew666 <yjliew455@gmail.com>
2023-11-07 22:47:50 +0800

    Login page changes

--- src/components/Homepage/Login.js
+++ src/components/Homepage/Login.js
@@ -10,7 +10,10 @@ class LoginPage extends React.Component {
           <div className="blue-rectangle">
             <img src={logo1} alt="Big Logo" className="big-logo" />
             <div className="headline">
-              Your Future Starts Here: Explore Opportunities with AiVides
+              Your Future Starts Here:
+            </div>
+            <div className="headline2">
+              Explore Opportunities with AiVides
             </div>
             <div className="subheadline">
               Your One-Stop Solution for Streamlining the Job Search and Hiring Process. Discover a World of Opportunities for Jobseekers, and Effortless Talent Acquisition for HR Managers. We're Here to Simplify Your Path to Success, Every Step of the Way.
