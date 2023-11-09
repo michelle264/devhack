1436695a1d4d9a581521f9f4d242be4e87266f97
yjliew666 <yjliew455@gmail.com>
2023-11-09 19:47:05 +0800

    Register page changes

--- src/components/Homepage/Register.js
+++ src/components/Homepage/Register.js
@@ -3,13 +3,7 @@ import './Register.css';
 import { Link } from 'react-router-dom';
 
 class RegisterPage extends React.Component {
-  state = {
-    selectedRole: null,
-  };
 
-  handleRoleSelection = (role) => {
-    this.setState({ selectedRole: role });
-  };
     render() {
       return (
         <div className="register-page">
@@ -26,23 +20,6 @@ class RegisterPage extends React.Component {
               <input type="text" className="text-input" />
             </div>
             <div className="form-group">
-        <label>Role</label>
-        <div className="role-selection">
-          <button
-            className={`role-button ${this.state.selectedRole === 'hr' ? 'selected' : ''}`}
-            onClick={() => this.handleRoleSelection('hr')}
-          >
-            HR
-         </button>
-         <button
-           className={`role-button ${this.state.selectedRole === 'jobSeeker' ? 'selected' : ''}`}
-            onClick={() => this.handleRoleSelection('jobSeeker')}
-         >
-           Job Seeker
-          </button>
-        </div>
-      </div>
-            <div className="form-group">
               <label>Password</label>
               <input type="password" className="text-input" />
             </div>
@@ -50,7 +27,12 @@ class RegisterPage extends React.Component {
               <label>Confirm Password</label>
               <input type="password" className="text-input" />
             </div>
-            <button className="register-button2">Register</button>
+            <Link to="/HRProfile">
+            <button className="register-button2">Register as HR</button>
+            </Link>
+            <Link to="/listjobjs">
+            <button className="register-button2">Register as Job Seeker</button>
+          </Link>
             <div className="login-text2">Or Login using</div>
             <div className="login-button2">
             <Link to="/login">
