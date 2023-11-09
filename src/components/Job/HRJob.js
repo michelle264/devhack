54bf224d86da62d6fdc7bfbdcc9789fdda250264
Michelle Hu Ynh Tin <116173980+michelle264@users.noreply.github.com>
2023-11-09 19:42:01 +0800

    update HRJob

--- src/components/Job/HRJob.js
+++ src/components/Job/HRJob.js
@@ -15,7 +15,8 @@ class HRJob extends React.Component{
                 <div className='box'>
                     <div className='card-left'>
                         <div className='card-title'>Principal Engineer Data Scientist</div>
-                        <div className='card-details'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Bibendum vitae dictumst sit vitae, mi imperdiet sit. Lectus eleifend aliquam nibh mauris, pretium. Lectus magnis lorem massa urna felis porta.</div> 
+                        <div className='card-details'> As a Principal Engineer - Data Scientist at Intel, you will play a crucial role in shaping our data science and engineering strategy, driving innovation, and contributing to the development of cutting-edge solutions.You will lead a team of data scientists and engineers, collaborating with cross-functional teams to develop and deploy data-driven applications, machine learning models, and advanced analytics solutions.
+         You will also be responsible for setting the technical direction and providing expertise in data science and engineering.</div> 
                         <Link to="/Resume">
                         <button className='check-button'>CHECK</button>
                         </Link>
@@ -25,8 +26,8 @@ class HRJob extends React.Component{
 
                 <div className='box'>
                     <div className='card-left'>
-                        <div className='card-title'>Principal Engineer Data Scientist</div>
-                        <div className='card-details'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Bibendum vitae dictumst sit vitae, mi imperdiet sit. Lectus eleifend aliquam nibh mauris, pretium. Lectus magnis lorem massa urna felis porta.</div> 
+                        <div className='card-title'>Backend Programmer</div>
+                        <div className='card-details'>Responsible for designing, developing, and maintaining the server-side components of a software application or website. They work with databases, server infrastructure, and application logic to ensure that the system functions efficiently and securely, often focusing on the functionality and performance of the application without being directly visible to end users.</div> 
                         <Link to="/Resume">
                         <button className='check-button'>CHECK</button>
                         </Link>
@@ -36,8 +37,8 @@ class HRJob extends React.Component{
 
                 <div className='box'>
                     <div className='card-left'>
-                        <div className='card-title'>Principal Engineer Data Scientist</div>
-                        <div className='card-details'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Bibendum vitae dictumst sit vitae, mi imperdiet sit. Lectus eleifend aliquam nibh mauris, pretium. Lectus magnis lorem massa urna felis porta.</div> 
+                        <div className='card-title'>Frontend Programmer</div>
+                        <div className='card-details'>Responsible for creating the visual and interactive elements of a software application or website that users directly interact with. They work on the user interface (UI), design, and client-side scripting to ensure a responsive and visually appealing user experience. Frontend programmers use HTML, CSS, JavaScript, and various frontend frameworks to build the presentation layer of web and mobile applications.</div> 
                         <Link to="/Resume">
                         <button className='check-button'>CHECK</button>
                         </Link>
--- src/components/profile/HRProfile.js
+++ src/components/profile/HRProfile.js
@@ -9,69 +9,69 @@ import person3 from './person3.png';
 class ProfileComponent extends React.Component {
     render() {
       return (
-        <div className="All">
-          <div className="Container">
-        <div className="Left">
-          <div className="Rectangles-container">
-            <img src={rectangleImage} alt="Rectangle 1" className="rectangle1" />
-            <div className="Rectangle2">
-            <div className="Profile-container">
+        <div className="All2">
+          <div className="Container2">
+        <div className="Left2">
+          <div className="Rectangles-container2">
+            <img src={rectangleImage} alt="Rectangle 1" className="Rectangle12" />
+            <div className="Rectangle22">
+            <div className="Profile-container2">
               <img src={profileImage} alt="profile"/>
               </div>
-            <button className="Edit-button">Edit</button>
-            <div className="Name">Mr Liew Jun Xi</div>
-            <div className="Position">HR</div>
-            <div className="Company">Company:</div>
-            <div className="Fill">Dell Inc.</div>
-            <div className="Location">Location:</div>
-            <div className="Fill2">Klang, Selangor</div>
+            <button className="Edit-button2">Edit</button>
+            <div className="Name2">Mr Liew Jun Xi</div>
+            <div className="Position2">HR</div>
+            <div className="Company2">Company:</div>
+            <div className="Fill2">Dell Inc.</div>
+            <div className="Location2">Location:</div>
+            <div className="Fill22">Klang, Selangor</div>
             
             </div>
           </div>
   
-          <div className="Rectangle3">
-            <div className="SameRow">
-            <div className="Title">General information</div>
-            <button className="Edit-button2">Edit</button>
+          <div className="Rectangle32">
+            <div className="SameRow2">
+            <div className="Title2">General information</div>
+            <button className="Edit-button22">Edit</button>
             </div>
-            <div className="Email">Email</div>
-            <div className="Email-address">liew123@gmail.com</div>
-            <div className="Country">Country/Region</div>
-            <div className="Email-address">Malaysia</div>
-            <div className="Country">Phone Number</div>
-            <div className="Email-address">+60183643746</div>
+            <div className="Email2">Email</div>
+            <div className="Email-address2">liew123@gmail.com</div>
+            <div className="Country2">Country/Region</div>
+            <div className="Email-address2">Malaysia</div>
+            <div className="Country2">Phone Number</div>
+            <div className="Email-address2">+60183643746</div>
           </div>
           
-          <div className="Rectangle4">
-            <div className="Title">View Applicants</div>
-            <button className="Edit-button3">View Full Lists</button>
+          <div className="Rectangle42">
+            <div className="Title2">View Applicants</div>
+            <button className="Edit-button32">View Full Lists</button>
           </div>
 
           </div>
-          <div className="Right">
-            <div className="Title1">YOU MAY LIKE THESE JOB SEEKERS</div>
-            <div className="Samerow3">
-              <div className="Image">
+          <div className="Right2">
+            <div className="Title12">YOU MAY LIKE THESE JOB SEEKERS</div>
+            <div className="Samerow32">
+              <div className="Image2">
             <img src={person1} alt="person1"/>
             </div>
-            <div className="Person1">Wong Suk Yen</div>
+            <div className="Person12">Wong Suk Yen</div>
             </div>
   
-            <div className="Samerow3">
-              <div className="Image2">
+            <div className="Samerow32">
+              <div className="Image22">
             <img src={person2} alt="person2"/>
             </div>
-            <div className="person2">Michael James Anderson</div>
+            <div className="person22">Michael James Anderson</div>
             </div>
   
-            <div className="Samerow3">
-              <div className="Image3">
+            <div className="Samerow32">
+              <div className="Image32">
             <img src={person3} alt="person3"/>
             </div>
-            <div className="Person3">David Robert Williams</div>
+            <div className="Person32">David Robert Williams</div>
             </div>
   
-            <div className="Recommendation">
+            <div className="Recommendation2">
               SEE ALL RECOMMENDATIONS
             </div>
             
